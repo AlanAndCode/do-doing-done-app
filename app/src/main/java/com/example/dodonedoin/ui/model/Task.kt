00b821109e4269657cbb887143b29f1b5a477a96
Alan Anderson <64568493+AlanAndCode@users.noreply.github.com>
2023-04-24 17:14:17 -0300

    feat: adding task model and task adapter

---
+++ app/src/main/java/com/example/dodonedoin/ui/model/Task.kt
@@ -0,0 +1,17 @@
+package com.example.dodonedoin.ui.model
+import android.os.Parcelable
+import com.example.dodonedoin.helper.FirebaseHelper
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Task(
+    var id: String = "",
+    var hour:String = "",
+    var minutes:String = "",
+    var description: String = "",
+    var status: Int = 0
+) : Parcelable {
+    init {
+        this.id = FirebaseHelper.getDatabase().push().key ?: ""
+    }
+}
